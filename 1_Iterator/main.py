744474dcfe017026f341485e486559bf68714976
1616imuimu <yagasaki.kei@gmail.com>
2023-10-13 23:20:26 +0900

    Aggregateインタフェースを作成

--- 1_Iterator/main.py
+++ 1_Iterator/main.py
@@ -1,5 +1,18 @@
 """Iteratorパターンのサンプルプログラム
 """
+import abc
+
+
+class IAggregate(metaclass=abc.ABCmeta):
+    """_summary_
+
+    Args:
+        metaclass (_type_, optional): _description_. Defaults to abc.ABCmeta.
+    """
+    @abc.abstractmethod
+    def iterator(self) -> Iterator:
+        """_summary_
+        """
 
 
 def main():
