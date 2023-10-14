2233488ca5040a1e230cbf2f0d6041c23df42efb
1616imuimu <yagasaki.kei@gmail.com>
2023-10-14 23:33:56 +0900

    Bannerクラスを追加

--- 2_Adapter/main.py
+++ 2_Adapter/main.py
@@ -2,6 +2,24 @@
 """
 
 
+class Banner():
+    """_summary_
+    """
+
+    def __init__(self, string: str) -> None:
+        self.__string: str = string
+
+    def show_with_paren(self) -> None:
+        """_summary_
+        """
+        print(f"({self.__string})")
+
+    def show_with_aster(self) -> None:
+        """_summary_
+        """
+        print(f"*{self.__string}*")
+
+
 def main():
     """main関数
     """
